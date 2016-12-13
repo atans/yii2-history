b03f2accd498001bbf14380c4c682d543a260b73
Atans Chiu <atans.chiu@gmail.com>
2016-12-13 21:04:32 +0800

    Save changes

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ The preferred way to install this extension is through [composer](http://getcomp
 Either run
 
 ```
-composer atans/yii2-history "*"
+composer require atans/yii2-history
 ```
 
 or add
--- migrations/m161213_174900_init.php
+++ migrations/m161213_174900_init.php
@@ -7,14 +7,21 @@ class m161213_174900_init extends \atans\history\migrations\Migration
     {
         $this->createTable('{{%activerecord_history}}', [
             'id'         => $this->primaryKey(),
-            'table'      => $this->string('50')->notNull(),
-            'event'      => $this->string('50')->notNull(),
-            'scenario'   => $this->string('50')->notNull(),
-            'key'        => $this->string('32')->notNull(),
+            'table'      => $this->string(50)->notNull(),
+            'event'      => $this->string(50)->notNull(),
+            'scenario'   => $this->string(50)->notNull(),
+            'key'        => $this->string(32)->notNull(),
             'data'       => $this->text()->notNull(),
             'ip'         => $this->string(42)->null(),
+            'created_by' => $this->string(42)->null(),
             'created_at' => $this->dateTime()->notNull(),
         ], $this->tableOptions);
+
+        $this->createIndex('table', '{{%activerecord_history}}', ['table']);
+        $this->createIndex('event', '{{%activerecord_history}}', ['event']);
+        $this->createIndex('scenario', '{{%activerecord_history}}', ['scenario']);
+        $this->createIndex('key', '{{%activerecord_history}}', ['key']);
+        $this->createIndex('created_by', '{{%activerecord_history}}', ['created_by']);
     }
 
     public function down()
